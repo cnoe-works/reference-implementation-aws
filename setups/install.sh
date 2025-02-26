9a42245a3b039040846234cb0aebea6bed8d0c9d
Pankaj Walke <pankaj.walke@mavs.uta.edu>
2025-02-26 22:22:37 +0000

    Reset ref-implementation to default

--- setups/install.sh
+++ setups/install.sh
@@ -20,7 +20,29 @@ if [[ ! "$response" =~ ^[Yy][Ee][Ss]$ ]]; then
   exit 0
 fi
 
+# Substitute Github Organization/User name in files.
 export GITHUB_URL=$(yq '.repo_url' ./setups/config.yaml)
+export HOSTED_ZONE_ID=$(yq '.hosted_zone_id' ./setups/config.yaml)
+export GITHUB_ORG=$(echo "${GITHUB_URL}" | sed 's/\/[^/]*$//')
+export BASE_DOMAIN_NAME=$(aws route53 get-hosted-zone \
+                      --id "${HOSTED_ZONE_ID}" --query 'HostedZone.Name' \
+                      --output text | sed 's/\.$//')
+
+# Update Backstage config with Github URL
+sed -i "s|GITHUB_URL|${GITHUB_URL}|g" "${REPO_ROOT}/packages/backstage/dev/cm-backstage-config.yaml"
+
+find "${REPO_ROOT}/packages/backstage-templates" -type f -name "*.yaml" | while read file; do
+    sed -i \
+        -e "s|GITHUB_URL|${GITHUB_URL}|g" \
+        -e "s|GITHUB_ORG|${GITHUB_ORG}|g" \
+        -e "s|BASE_DOMAIN_NAME|${BASE_DOMAIN_NAME}|g" \
+        "$file"
+done
+
+# Push modified files to Github
+cd "${REPO_ROOT}"
+git add . && git commit -m "Updated config files for reference implementation" && git push origin main
+cd -
 
 # Set up ArgoCD. We will use ArgoCD to install all components.
 cd "${REPO_ROOT}/setups/argocd/"
